d5c3d5a1b1f9914fe09ab04d783a9cfe10fc4a11
ttodawg01 <tonycampos1803@gmail.com>
2022-10-27 09:22:39 -0500

    fixed migrate

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app import db
 
 #creating a user class
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), unique=True, nullable=False)
     last_name = db.Column(db.String(120), unique=True, nullable=False)
